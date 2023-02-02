c1edae1867306ea56f92ed0409f0df496ea71d28
Alex Owusu <Alexander.c.owusu@gmail.com>
2023-02-02 21:59:05 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="app">
+    <!-- <img width="25%" src="./assets/logo.png">
+    -->
+    <!-- <icon /> -->
+    <!-- <settings /> -->
+    <timer />
+    <!-- <el-button icon="el-icon-s-tools" v-on:click="showDialog()" circle></el-button> -->
+  </div>
+</template>
+
+<script>
+import settings from "./components/settings";
+
+import timer from "./components/timer";
+import icon from "./assets/clip.vue";
+export default {
+  name: "App",
+  components: {
+    settings,
+    timer,
+    icon,
+  },
+  methods: {
+    showDialog() {
+      console.log("shown");
+      this.$store.dispatch("showMenu");
+    },
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.cdnfonts.com/css/montserrat.css");
+@import url("https://fonts.cdnfonts.com/css/montserrat.css");
+
+.slides {
+  font-family: "Montserrat Light";
+}
+
+.slides h1,
+.slides h2,
+.slides h3,
+.slides h4,
+.slides h5,
+.slides h6 {
+  font-family: "Montserrat", sans-serif;
+}
+
+#app {
+  background: #002147;
+  /* background-image: linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%); */
+  font-family: "Montserrat", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #e26c7e;
+  margin: 0px;
+  padding: 20px;
+  height: 100%;
+}
+body,
+html {
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  overflow: hidden;
+}
+</style>
---
+++ src/components/timer.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="hello">
+    <h2>{{ time }}</h2>
+    <!-- {{ getSeconds }} -->
+    <!-- <code>{{ getTask.name }}</code> -->
+    <div></div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "HelloWorld",
+
+  created() {
+    this.$store.dispatch("reset");
+  },
+  computed: {
+    ...mapGetters(["getSeconds", "getTask"]),
+    time() {
+      let totalSeconds = this.getSeconds;
+      let hours = Math.floor(this.getSeconds / 3600);
+      totalSeconds %= 3600;
+      let minutes = Math.floor(totalSeconds / 60);
+      let seconds = totalSeconds % 60;
+      return `${hours}:${minutes}:${seconds}`;
+    },
+
+    // getSeconds() {
+    //   return this.$store.getters.getSeconds;
+    // },
+    // getName() {
+    //   return this.$store.getters.getTask.name;
+    // }
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+.hello {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+h2 {
+  font-size: 20em;
+  /* margin: 20px; */
+}
+code {
+  font-size: 5em;
+}
+button {
+  background: #ffffff;
+  border: 0;
+  padding: 1em;
+  border-radius: 50%;
+}
+</style>
